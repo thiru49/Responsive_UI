b0a59d23cceacf1f68bcad4f2d16dfa1fe04c9f6
thiru49 <thiruppathi497@gmail.com>
2023-11-18 18:08:33 +0530

    fix

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,8 +1,8 @@
 import Blog from "./blog/Blog.jsx";
-import Features  from './features/Feature'
-import Footer from "./footer/footer";
-import Header from "./header/Header";
-import Possibility from "./possibility/possibility";
-import WhatGPT from "./whatGPT/WhatGPT";
+import Features  from './features/Feature.jsx'
+import Footer from "./footer/footer.jsx";
+import Header from "./header/Header.jsx";
+import Possibility from "./possibility/Possibility.jsx";
+import WhatGPT from "./whatGPT/WhatGPT.jsx";
 
 export {Blog,Features,Footer,Header,Possibility,WhatGPT}
